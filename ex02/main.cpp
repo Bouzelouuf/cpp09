ad19d11ea81715e4aa3f87bfedff525a5a24aef4
Abdallah <abdallah@gmail.com>
2025-11-24 00:19:44 +0100

    1

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,12 +1,12 @@
-#include "PmergeMe.hpp"
-#include <iostream>
-
-int main(int ac,char **av)
+int main(int ac, char **av)
 {
-	if (ac >= 2)
+	if (ac < 2)
 	{
-		PmergeMe test;
-		test.sort(ac, av);
-		//test._sortVector(test._vec);
+		std::cerr << "Error: please provide at least one positive integer" << std::endl;
+		return 1;
 	}
+	
+	PmergeMe test;
+	test.sort(ac, av);
+	return 0;
 }
---
+++ ex02/test2.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <stdint.h>
+#include <netinet/in.h> 
+
+typedef struct s_clients
+{
+    int id;
+    char msg[1024];
+}   t_clients;
+
+int idNext = 0;
+int fdMax = 0;
+char bufferRead[12000];
+char bufferWrite[12000];
+fd_set readfds, writefds, active;
+t_clients clients[1024];
+
+void ftError(char *str)
+{
+    if (str)
+        write(2, str, strlen(str));
+    else
+        write(2, "Fatal error", 11);
+    write(2, "\n", 1);
+    exit(1);
+}
+
+void sendAll(int not)
+{
+    for (int i = 0; i <= fdMax; i++)
+    {
+        if (FD_ISSET(i, &writefds) && i != not)
+            send(i ,bufferWrite, strlen(bufferWrite), 0);
+    }
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        ftError("Wrong number of arguments");
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        ftError(NULL);
+    FD_ZERO(&active);
+    bzero(&clients, sizeof(clients));
+    fdMax = sockfd;
+    FD_SET(sockfd, &active);
+    struct sockaddr_in servaddr;
+    bzero(&servaddr, sizeof(servaddr));
+    socklen_t len;
+    servaddr.sin_addr.s_addr = 0x0100007F;
+    servaddr.sin_family = AF_INET;
+    uint16_t port = atoi(av[1]);
+    servaddr.sin_port = ((port &0xFF) << 8) | ((port >> 8) &0xFF);
+    if (bind(sockfd, (const struct sockaddr*) &servaddr, sizeof(servaddr)) < 0)
+        ftError(NULL);
+    if (listen(sockfd, 10) < 0)
+        ftError(NULL);
+    while(1)
+    {
+        if (select(sockfd + 1, &readfds, &writefds, NULL , NULL) < 0)
+            continue;
+        for (int fdI = 0 ; fdI<= fdMax; fdI++)
+        {
+            if (FD_ISSET(fdI, &readfds) && fdI == sockfd)
+            {
+                len = sizeof(servaddr);
+                int connfd = accept(sockfd, (struct sockaddr*) &servaddr, &len);
+                if (connfd < 0)
+                    continue;
+                fdMax = connfd > fdMax ? connfd : fdMax;
+                clients[connfd].id = idNext++;
+                sprintf(bufferWrite, "server: client %d just arrived\n", clients[connfd].id);
+                sendAll(connfd);
+                FD_SET(connfd, &active);
+                break;
+            }
+            if (FD_ISSET(fdI, &readfds) && fdI != sockfd)
+            {
+                int res = recv(fdI, bufferRead, sizeof(bufferRead) - 1, 0);
+                if (res <= 0)
+                {
+                    sprintf(bufferWrite, "server: client %d just left\n", clients[fdI].id);
+                    sendAll(fdI);
+                    FD_CLR(fdI, &active);
+                    close(fdI);
+                    break;
+                }
+                else
+                {
+                    for (int i = 0; i < res; i++)
+                    {
+                        if (bufferRead[i] == '\n')
+                        {
+                            if (strlen(clients[fdI].msg) > 0)
+                            {
+                                sprintf(bufferWrite,"client %d: %s\n",clients[fdI].id, clients[fdI].msg);
+                                sendAll(fdI);
+                            }
+                            memset(clients[fdI].msg, 0 , sizeof(clients[fdI].msg));
+                        }
+                        else
+                        {
+                            int len_msg = strlen(clients[fdI].msg);
+                            if (len_msg < 1023)
+                            {
+                                clients[fdI].msg[len] = bufferWrite[i];
+                                clients[fdI].msg[len + 1] = '\0'; 
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
